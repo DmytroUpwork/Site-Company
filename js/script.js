a29b216f1c00ba6eb1e1e1d2fce7ef1504167dd1
Dmitrii <lomonovskij@gmail.com>
2021-01-01 22:41:25 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-
+$(document).ready(function(){   
+  var target = $(".blue-circle-voucher");
+  var targetPos = target.offset().top;
+  var winHeight = $(window).height();
+  var scrollToElem = targetPos - winHeight;
+  $(window).scroll(function () {
+    var winScrollTop = $(this).scrollTop();
+    var winScrollBottom =
+      $(this).scrollTop() - $(window).height() - $(target).height();
+    if (winScrollTop > scrollToElem && winScrollBottom < scrollToElem) {
+      $(".icon").addClass("active-color-svg");
+      $(".decor-line, .navbar-toggler-icon").addClass("active-color");
+    } else {
+      $(".icon").removeClass("active-color-svg");
+      $(".decor-line, .navbar-toggler-icon").removeClass("active-color");
+    }
+  });
+});

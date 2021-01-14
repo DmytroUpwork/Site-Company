d63ce0b53ad3c417741a555ad9d6dfa99452c881
Dmitrii <lomonovskij@gmail.com>
2021-01-14 13:44:01 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,48 +1,72 @@
 $(document).on( "ready", function(){
   // Смена цвета иконок бокового меню при скролле 
-  var target1 = $(".blue-circle-voucher");
-  var target2 = $(".sec-form");
-  var targetPos1 = target1.offset().top;
-  var targetPos2 = target2.offset().top;
-  var winHeight = $(window).height();
-  var scrollToElem1 = targetPos1 - winHeight;
-  var scrollToElem2 = targetPos2 - winHeight;
+  
+
+
   $(window).on("scroll", function () {
+
+    var target1 = $(".blue-circle-voucher");
+    var target2 = $(".sec-form");
+    var targetPos1 = target1.offset().top;
+    var targetPos2 = target2.offset().top;
+    var winHeight = $(window).height();
+
     var winScrollTop = $(this).scrollTop();
-    var winScrollBottom1 =
-      $(this).scrollTop() - $(window).height() - $(target1).height();
-    var winScrollBottom2 =
-      $(this).scrollTop() - $(window).height() - $(target2).height();
-    if (
-      (winScrollTop > scrollToElem1 && winScrollBottom1 < scrollToElem1 || winScrollTop > scrollToElem2 && winScrollBottom2 < scrollToElem2) 
-    ) {
-      $(".icon-svg").attr("class", "icon-svg active-color-svg");
-      $(".navbar-toggler").attr("class", "navbar-toggler active-color");
-      $(".decor-line").attr("class", "decor-line active-color");
-    } else {
-      $(".icon-svg").attr("class", "icon-svg");
-      $(".navbar-toggler").attr("class", "navbar-toggler");
-      $(".decor-line").attr("class", "decor-line");
-    }
-    // Анимация элемениа фиксированного меню
-    var top = window.pageYOffset;
-    if (scroll < top) {
-      $(".decor-line").attr("class", "decor-line active-animate-svg-bottom");
-    } else if (scroll > top) {
-      $(".decor-line").attr("class", "decor-line active-animate-svg-top");
-    }
-    // Появление и скрытие кнопки выпадающего меню
-    if (window.innerWidth > 992) {
-      if (winScrollTop > 100) {
-        $(".navbar-toggler").attr("class", "navbar-toggler t-navbar-toggler but-navbar-z-index");
-      } else {
-        $(".navbar-toggler").attr("class", "navbar-toggler t-navbar-toggler");
+    var winScrollBottom1 = $(this).scrollTop() - winHeight - $(target1).height();
+    var winScrollBottom2 = $(this).scrollTop() - winHeight - $(target2).height();
+    
+    var scrollToElem1 = targetPos1 - winHeight;
+    var scrollToElem2 = targetPos2 - winHeight;
+
+    // var top = window.pageYOffset;
+
+    function colorMenu() {
+      if ( (winScrollTop > scrollToElem1 && winScrollBottom1 < scrollToElem1) || (winScrollTop > scrollToElem2 && winScrollBottom2 < scrollToElem2) ) {
+        $(".icon-svg").attr("class", "icon-svg active-color-svg");
+        $(".navbar-toggler span").attr("class", "active-color");
+        $(".decor-line").attr("class", "decor-line active-color");
+      } 
+      else {
+        $(".icon-svg").attr("class", "icon-svg");
+        $(".navbar-toggler span").attr("class", "");
+        $(".decor-line").attr("class", "decor-line");
       }
-    } 
-    else {
-      $(".navbar-toggler").attr("class", "navbar-toggler t-navbar-toggler but-navbar-z-index");
-    } 
+    }
+    colorMenu();
+    
+    
+    function butMenuHideShow() {
+      if (window.innerWidth > 992) {
+        if (winScrollTop > 100) {
+          $(".navbar-toggler").addClass("but-navbar-z-index");
+        } else {
+          $(".navbar-toggler").removeClass("but-navbar-z-index");
+        }
+      }
+    }
+    butMenuHideShow();
+    
+
+    
+    function onScroll() {
+      var top = window.pageYOffset;
+      if (scroll < top) {
+        $(".decor-line").addClass("active-animate-svg-bottom");
+      } else if (scroll > top) {
+        $(".decor-line").addClass("active-animate-svg-top");
+      } 
+      scroll = top;
+    }
+    onScroll();
+    
+    
+
+
   });
+
+
+
+
   // Код закрытия и открытия меню при нажатии на элемент + его скрытие при клике на документ
   $(".navbar-toggler").click(function (e) {
     $(this).addClass("navbar-toggler-active");

6045f6913c3d5970d6a29eee2611030f60e223e6
Dmitrii <lomonovskij@gmail.com>
2021-01-19 18:24:38 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -5,25 +5,39 @@ $(document).on( "ready", function(){
 
     var winScrollTop = $(this).scrollTop();
     function colorMenu() {
-      var target1 = $(".blue-circle-voucher");
-      var target2 = $(".sec-form");
-      var targetPos1 = target1.offset().top;
-      var targetPos2 = target2.offset().top;
-      var winHeight = $(window).height();
-      var winScrollBottom1 = $(this).scrollTop() - winHeight - $(target1).height();
-      var winScrollBottom2 = $(this).scrollTop() - winHeight - $(target2).height();
-      var scrollToElem1 = targetPos1 - winHeight;
-      var scrollToElem2 = targetPos2 - winHeight;
-      if ( (winScrollTop > scrollToElem1 && winScrollBottom1 < scrollToElem1) || (winScrollTop > scrollToElem2 && winScrollBottom2 < scrollToElem2) ) {
-        $(".icon-svg").attr("class", "icon-svg active-color-svg");
-        $(".navbar-toggler span").addClass("active-color");
-        $(".decor-line").attr("class", "decor-line active-color");
-      } 
-      else {
-        $(".icon-svg").attr("class", "icon-svg");
-        $(".navbar-toggler span").removeClass("active-color");
-        $(".decor-line").attr("class", "decor-line");
+
+      if (window.innerWidth > 992) {
+
+        var target1 = $(".blue-circle-voucher");
+        var target2 = $(".sec-form");
+        var targetPos1 = target1.offset().top;
+        var targetPos2 = target2.offset().top;
+        var winHeight = $(window).height();
+        var winScrollBottom1 =
+          $(this).scrollTop() - winHeight - $(target1).height();
+        var winScrollBottom2 =
+          $(this).scrollTop() - winHeight - $(target2).height();
+        var scrollToElem1 = targetPos1 - winHeight;
+        var scrollToElem2 = targetPos2 - winHeight;
+        if (
+          (winScrollTop > scrollToElem1 && winScrollBottom1 < scrollToElem1) ||
+          (winScrollTop > scrollToElem2 && winScrollBottom2 < scrollToElem2)
+        ) {
+          $(".icon-svg").attr("class", "icon-svg active-color-svg");
+          $(".navbar-toggler span").addClass("active-color");
+          $(".decor-line").attr("class", "decor-line active-color");
+        } else {
+          $(".icon-svg").attr("class", "icon-svg");
+          $(".navbar-toggler span").removeClass("active-color");
+          $(".decor-line").attr("class", "decor-line");
+        }
+
+
       }
+
+      
+
+
     }
 
     function butMenuHideShow() {
@@ -75,6 +89,22 @@ $(document).on( "ready", function(){
     });
   };
   menuHideShow();
+
+
+  // Плавная прокрутка якорей
+  $("a[href*='#']").on("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+    }, 777);
+    e.preventDefault();
+    return false;
+  });
+
+
+
+
+
   
   // Слайдер
   $(".center").slick({
